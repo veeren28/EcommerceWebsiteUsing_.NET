22dda481557e81bf9ece63788d6924841ff62b18
veeren28 <veerenveerupinisetty@gmail.com>
2025-09-09 23:28:46 +0530

    working on models , dont trust

--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -1,3 +1,6 @@
+using Ecommerce.data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen(options => { options.EnableAnnotations(); });
+builder.Services.AddDbContext<AppContextDb>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
+
+
+
 
 
 
